6c69b996378c52651c46415c5f08587d7b281183
iamashwini78 <ashwini@blisstech.in>
2025-07-04 17:40:42 +0530

    fix: fixed issue of button component

--- resources/js/pages/settings/Password.vue
+++ resources/js/pages/settings/Password.vue
@@ -6,7 +6,7 @@ import { Head, useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 import HeadingSmall from '@/components/HeadingSmall.vue';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { type BreadcrumbItem } from '@/types';
